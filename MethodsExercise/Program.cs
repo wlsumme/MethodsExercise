e1991b5feefae71c04c8fbc915035380b0801786
William Summe <wlsumme@gmail.com>
2024-02-16 15:06:50 -0600

    Methods Added

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,88 @@
-﻿namespace MethodsExercise
+﻿
+using System.ComponentModel;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLib();
+
+            var add = Add(2, 4);
+            Console.WriteLine(add);
+
+            int mult = Multiply(10, 2);
+            Console.WriteLine(mult);
+
+            int sub = Subtract(60, 23);
+            Console.WriteLine(sub);
+
+            int div = Divide(1000, 50);
+            Console.WriteLine(div);
+
+
+
+
+            
+
+            
+
+            
         }
-    }
+        public static void MadLib()
+        {
+            Console.WriteLine("Exercise 1\n");
+            Console.WriteLine("What is you name?");
+            var name = Console.ReadLine();
+            Console.WriteLine("What is your favorite color?");
+            var color = Console.ReadLine();
+            Console.WriteLine("What is your favorite animal?");
+            var animal = Console.ReadLine();
+            Console.WriteLine("What is your favorite car?");
+            var car = Console.ReadLine();
+            Console.WriteLine($" I'm {name}. Today i saw a {animal}, taking a {color} poop on a fancy {car}.");
+
+            Console.WriteLine("\n\nExercise 2\n");
+
+
+        }
+        public static int Add(int num1, int num2)
+        
+        {
+            return num1 + num2;
+        }
+        public static int Multiply(int num1, int num2)
+        
+        {
+            return num1 * num2;
+        }
+        public static int Subtract(int num1, int num2) 
+        
+        {
+            return num1 - num2;
+        }
+
+        public static int Divide(int num1, int num2) 
+        {
+            return num1 / num2;
+
+        }
+
+
+
+
+
+
+       }
 }
+
+
+
+
+
+
+
+
+
+
--- MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
+++ MethodsExercise/obj/Debug/net6.0/MethodsExercise.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+472d02f9d75aac5a20b813b93fd9e5787f6c5186")]
 [assembly: System.Reflection.AssemblyProductAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MethodsExercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
